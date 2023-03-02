65ee77486f77e6d66daa345bb111cf6d44c8b21c
psdon <psdonium@gmail.com>
2023-03-02 17:47:02 +0800

    updated business logic rules

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/common.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/common.py
@@ -1,6 +1,6 @@
 """
 1. Business logic only with dependency to repository service.
 2. Session object should use dependency injection to ease unit testing.
-3. Should not raise Exception related to a web framework (i.e. FastAPI HTTPException), let the api folder handle that.
-4. function parameters should be flat, no type hinting based on Pydantic Schema for example
+4. Raise exceptions you've only written, exception handling will be done in API layer.
+5. Function parameters should be flat, no type hinting based on Pydantic Schema for example
 """
